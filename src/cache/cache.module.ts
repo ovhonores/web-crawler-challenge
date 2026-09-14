5145d530f5383e418569b29d866a03e11de3a0ab
Orlando <ovhonores@gmail.com>
2026-09-13 23:00:36 -0500

    feat(cache): configure Redis store with @keyv/redis

--- src/cache/cache.module.ts
+++ src/cache/cache.module.ts
@@ -1,4 +1,23 @@
 import { Module } from '@nestjs/common';
+import { CacheModule as NestCacheModule } from '@nestjs/cache-manager';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import KeyvRedis from '@keyv/redis';
 
-@Module({})
+@Module({
+  imports: [
+    NestCacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        stores: [
+          new KeyvRedis(
+            `redis://${config.get('redis.host')}:${config.get('redis.port')}`,
+          ),
+        ],
+        ttl: (config.get<number>('redis.ttl') ?? 0) * 1000,
+      }),
+      isGlobal: true,
+    }),
+  ],
+})
 export class CacheModule {}
