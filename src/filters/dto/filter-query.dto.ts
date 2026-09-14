1e413eae991b1afdefcb5cd8daec428a266c21bd
Orlando <ovhonores@gmail.com>
2026-09-14 08:55:56 -0500

    feat(swagger): add ApiPropertyOptional decorators to FilterQueryDto

--- src/filters/dto/filter-query.dto.ts
+++ src/filters/dto/filter-query.dto.ts
@@ -1,3 +1,4 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsEnum, IsInt, IsOptional, Min } from 'class-validator';
 
@@ -21,20 +22,24 @@ export enum SortOrder {
 }
 // Dinamic FilterQueryDto class to handle query parameters for filtering and sorting entries
 export class FilterQueryDto {
+  @ApiPropertyOptional({ example: 5, minimum: 0, default: 5 })
   @IsOptional()
   @Type(() => Number)
   @IsInt()
   @Min(0)
   words?: number = 5;
 
+  @ApiPropertyOptional({ enum: WordOperator, default: WordOperator.GT })
   @IsOptional()
   @IsEnum(WordOperator)
   operator?: WordOperator = WordOperator.GT;
 
+  @ApiPropertyOptional({ enum: SortBy, default: SortBy.COMMENTS })
   @IsOptional()
   @IsEnum(SortBy)
   sortBy?: SortBy = SortBy.COMMENTS;
 
+  @ApiPropertyOptional({ enum: SortOrder, default: SortOrder.DESC })
   @IsOptional()
   @IsEnum(SortOrder)
   order?: SortOrder = SortOrder.DESC;
