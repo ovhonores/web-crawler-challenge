eb03c20598109e14e838a28624d006862291cded
Orlando <ovhonores@gmail.com>
2026-09-13 21:19:41 -0500

    feat(filters): Fix identation

--- src/crawler/hn.parser.ts
+++ src/crawler/hn.parser.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import * as cheerio from 'cheerio';
 
 import type { Entry } from './interfaces/entry.interface';
 import { HN_SELECTORS } from './hn-selectors';
@@ -8,10 +7,12 @@ import { EntryParser } from './entry.parser';
 @Injectable()
 export class HnParser {
   parseEntries(html: string, maxEntries: number): Entry[] {
-    const $ = cheerio.load(html);
     const entryParser = new EntryParser();
-    const entries: Entry[] = entryParser.parseEntries(html, maxEntries, HN_SELECTORS);
-   
+    const entries: Entry[] = entryParser.parseEntries(
+      html,
+      maxEntries,
+      HN_SELECTORS,
+    );
     return entries;
   }
 }
